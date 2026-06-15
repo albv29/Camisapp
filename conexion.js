bf82920304d25df0fcc926b9c4d10d737c3333bb
Alonso <2022015@est.cedesdonbosco.ed.cr>
2026-06-15 15:57:28 -0600

    Primer commit del proyecto Node.js

---
+++ conexion.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const uri = 'mongodb+srv://mcedes:Clavecita2026@cluster0.zisfazb.mongodb.net/camisApp';
+
+async function conectarBD() {
+    try {
+        await mongoose.connect(uri);
+        console.log('Conectado correctamente a MongoDB Atlas con Mongoose actualizado');
+    } catch (error) {
+        console.log('Error conectando con Mongoose: ', error.message);
+    }
+}
+
+module.exports = conectarBD;
